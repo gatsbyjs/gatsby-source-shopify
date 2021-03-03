3b79dd6de5d33414d9dba25a49a3ba1a33b4cc35
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-03-03 08:33:23 -0600

    Remove gatsby-node-helpers (#50)
    * Remove gatsby-node-helpers
    
    * Simplify buildFromId
    
    * Delete unnecessary layers

--- plugin/src/node-builder.ts
+++ plugin/src/node-builder.ts
@@ -1,5 +1,4 @@
 import { NodeInput, SourceNodesArgs } from "gatsby";
-import { IdentifiableRecord, NodeHelpers } from "gatsby-node-helpers";
 import { createRemoteFileNode } from "gatsby-source-filesystem";
 
 // 'gid://shopify/Metafield/6936247730264'
@@ -91,48 +90,37 @@ const processorMap: ProcessorMap = {
   },
 };
 
-async function buildFromId(
-  obj: Record<string, any>,
-  getFactory: (remoteType: string) => (node: IdentifiableRecord) => NodeInput,
-  gatsbyApi: SourceNodesArgs,
-  options: ShopifyPluginOptions
-) {
-  const [shopifyId, remoteType] = obj.id.match(pattern) || [];
-
-  const Node = getFactory(remoteType);
-  const processor = processorMap[remoteType] || (() => Promise.resolve());
-
-  attachParentId(obj);
-  const node = Node({ ...obj, id: shopifyId });
-  await processor(node, gatsbyApi, options);
-
-  return node;
-}
-
 export function nodeBuilder(
-  nodeHelpers: NodeHelpers,
   gatsbyApi: SourceNodesArgs,
   options: ShopifyPluginOptions
 ): NodeBuilder {
-  const factoryMap: {
-    [k: string]: (node: IdentifiableRecord) => NodeInput;
-  } = {};
-  const getFactory = (remoteType: string) => {
-    if (!factoryMap[remoteType]) {
-      factoryMap[remoteType] = nodeHelpers.createNodeFactory(remoteType, {
-        idIsGloballyUnique: true,
-      });
-    }
-    return factoryMap[remoteType];
-  };
-
   return {
-    async buildNode(obj: Record<string, any>) {
-      if (obj.id) {
-        return await buildFromId(obj, getFactory, gatsbyApi, options);
+    async buildNode(result: BulkResult) {
+      if (!pattern.test(result.id)) {
+        throw new Error(
+          `Expected an ID in the format gid://shopify/<typename>/<id>`
+        );
       }
 
-      throw new Error(`Cannot create a node without type information`);
+      const [, remoteType] = result.id.match(pattern) || [];
+
+      const processor = processorMap[remoteType] || (() => Promise.resolve());
+
+      attachParentId(result);
+
+      const node = {
+        ...result,
+        shopifyId: result.id,
+        id: gatsbyApi.createNodeId(result.id),
+        internal: {
+          type: `Shopify${remoteType}`,
+          contentDigest: gatsbyApi.createContentDigest(result),
+        },
+      };
+
+      await processor(node, gatsbyApi, options);
+
+      return node;
     },
   };
 }
