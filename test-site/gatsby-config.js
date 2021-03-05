373a8e881323d32f71631f289fb614f85e66bd05
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-03-05 10:51:30 -0600

    Only request active products

--- test-site/gatsby-config.js
+++ test-site/gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
         apiKey: process.env.SHOPIFY_ADMIN_API_KEY,
         password: process.env.SHOPIFY_ADMIN_PASSWORD,
         storeUrl: process.env.SHOPIFY_STORE_URL,
-        shopifyConnections: ["collections", "orders"],
+        shopifyConnections: ["collections"],
       },
     },
   ],
