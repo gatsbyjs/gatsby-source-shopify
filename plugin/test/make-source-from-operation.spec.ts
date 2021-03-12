7331c8edfcae4d9ba273afa2d74dd308a0ba13d9
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-03-12 13:57:13 -0600

    A test for getting canceled, retrying, and then succeeding

--- plugin/test/make-source-from-operation.spec.ts
+++ plugin/test/make-source-from-operation.spec.ts
@@ -1,4 +1,4 @@
-import { graphql } from "msw";
+import { graphql, rest } from "msw";
 import { setupServer } from "msw/node";
 import { SourceNodesArgs } from "gatsby";
 
@@ -21,6 +21,129 @@ afterEach(() => {
 
 afterAll(() => {
   server.close();
+});
+
+describe("When an operation gets canceled", () => {
+  const bulkResult = { id: "gid://shopify/Product/12345" };
+
+  beforeEach(() => {
+    server.use(
+      graphql.query<CurrentBulkOperationResponse>(
+        "OPERATION_STATUS",
+        (_req, res, ctx) => {
+          return res(
+            ctx.data({
+              currentBulkOperation: {
+                id: ``,
+                status: `COMPLETED`,
+              },
+            })
+          );
+        }
+      ),
+      graphql.mutation<BulkOperationRunQueryResponse>(
+        "INITIATE_BULK_OPERATION",
+        (_req, res, ctx) => {
+          return res(
+            ctx.data({
+              bulkOperationRunQuery: {
+                bulkOperation: {
+                  id: "",
+                  objectCount: "0",
+                  query: "",
+                  status: "CREATED",
+                  url: "",
+                },
+                userErrors: [],
+              },
+            })
+          );
+        }
+      ),
+      graphql.query<{ node: BulkOperationNode }>(
+        "OPERATION_BY_ID",
+        (_req, res, ctx) => {
+          return res.once(
+            ctx.data({
+              node: {
+                status: `CANCELED`,
+                id: "",
+                objectCount: "0",
+                query: "",
+                url: "",
+              },
+            })
+          );
+        }
+      ),
+      graphql.query<{ node: BulkOperationNode }>(
+        "OPERATION_BY_ID",
+        (_req, res, ctx) => {
+          return res(
+            ctx.data({
+              node: {
+                status: `COMPLETED`,
+                id: "",
+                objectCount: "1",
+                query: "",
+                url: "http://results.url",
+              },
+            })
+          );
+        }
+      ),
+      rest.get("http://results.url", (_req, res, ctx) => {
+        return res(ctx.text(JSON.stringify(bulkResult)));
+      })
+    );
+  });
+
+  it("tries again", async () => {
+    const createNode = jest.fn();
+    const gatsbyApiMock = jest.fn().mockImplementation(() => {
+      return {
+        cache: {
+          set: jest.fn(),
+        },
+        actions: {
+          createNode,
+        },
+        createContentDigest: jest.fn(),
+        createNodeId: jest.fn(),
+        reporter: {
+          info: jest.fn(),
+          error: jest.fn(),
+          panic: jest.fn(),
+          activityTimer: () => ({
+            start: jest.fn(),
+            end: jest.fn(),
+            setStatus: jest.fn(),
+          }),
+        },
+      };
+    });
+
+    const gatsbyApi = gatsbyApiMock as jest.Mock<SourceNodesArgs>;
+    const options = {
+      apiKey: ``,
+      password: ``,
+      storeUrl: "my-shop.shopify.com",
+    };
+    const operations = createOperations(options, gatsbyApi());
+
+    const sourceFromOperation = makeSourceFromOperation(
+      operations.finishLastOperation,
+      operations.completedOperation,
+      operations.cancelOperationInProgress,
+      gatsbyApi(),
+      options
+    );
+
+    await sourceFromOperation(operations.createProductsOperation);
+    expect(createNode).toHaveBeenCalledWith(
+      expect.objectContaining({ shopifyId: bulkResult.id })
+    );
+  });
 });
 
 describe("When an operation fails with bad credentials", () => {
