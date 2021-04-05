19f9ee245ff3216cb9d40c2e62390c187be92905
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-04-05 14:07:07 -0500

    Helpful comments

--- plugin/src/processors.ts
+++ plugin/src/processors.ts
@@ -10,10 +10,22 @@ export function collectionsProcessor(
   const promises = [];
   const collectionProductIndex: { [collectionId: string]: string[] } = {};
 
+  /**
+   * Read results in reverse so we can collect child node IDs.
+   * See Shopify Bulk Operation guide for more info.
+   *
+   * https://shopify.dev/tutorials/perform-bulk-operations-with-admin-api#download-result-data
+   */
   for (let i = objects.length - 1; i >= 0; i--) {
     const result = objects[i];
     const [id, remoteType] = result.id.match(idPattern) || [];
     if (remoteType === `Product`) {
+      /**
+       * We source products in a separate operation. Here we are
+       * just collecting product IDs so we can tell Gatsby about
+       * the many-to-many relationship between collections and
+       * products.
+       */
       if (!collectionProductIndex[result.__parentId]) {
         collectionProductIndex[result.__parentId] = [];
       }
