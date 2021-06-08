dcf1e9a06bbc5e48b7b88916d0b6ee24009db2e1
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2021-06-08 07:32:50 -0500

    Clean up orphaned metafield nodes attached to variants

--- plugin/src/processors/incremental-products.ts
+++ plugin/src/processors/incremental-products.ts
@@ -7,21 +7,44 @@ export function incrementalProductsProcessor(
   gatsbyApi: SourceNodesArgs,
   pluginOptions: ShopifyPluginOptions
 ): Promise<NodeInput>[] {
+  const { typePrefix = "" } = pluginOptions;
   const products = objects.filter((obj) => {
     const [, remoteType] = obj.id.match(idPattern) || [];
 
-    return remoteType === 'Product';
-  })
+    return remoteType === "Product";
+  });
 
-  const nodeIds = products.map(product => {
-    return createNodeId(product.id, gatsbyApi, pluginOptions)
-  })
+  const nodeIds = products.map((product) => {
+    return createNodeId(product.id, gatsbyApi, pluginOptions);
+  });
 
-  const variants = gatsbyApi.getNodesByType(`${pluginOptions.typePrefix || ``}ShopifyProductVariant`).filter((node) => nodeIds.includes(node.productId as string))
+  /**
+   * The events API doesn't tell us about deleted variants, so when we
+   * get the list of changed products, we have to compare those product
+   * variants with what we have in the cache, and delete those that are
+   * not present in the newer API results.
+   */
+  const variantsToDelete = gatsbyApi
+    .getNodesByType(`${typePrefix}ShopifyProductVariant`)
+    .filter((node) => nodeIds.includes(node.productId as string));
 
-  variants.forEach(variant => {
-    gatsbyApi.actions.deleteNode(variant)
-  })
+  variantsToDelete.forEach((variant) => {
+    gatsbyApi.actions.deleteNode(variant);
+  });
+
+  /**
+   * Additionally, product variants have metafields attached to them, so
+   * we must delete those as well to avoid oprhaned nodes building up in
+   * the cache.
+   */
+  const variantIds = variantsToDelete.map((v) => v.id);
+  gatsbyApi
+    .getNodesByType(`${typePrefix}ShopifyProductVariantMetafield`)
+    .forEach((metafield) => {
+      if (variantIds.includes(metafield.productVariantId as string)) {
+        gatsbyApi.actions.deleteNode(metafield);
+      }
+    });
 
   return objects.map(builder.buildNode);
 }
